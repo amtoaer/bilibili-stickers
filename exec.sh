6b93e596f2d447d2349dbaf2d2e81924d3c15318
amtoaer <amtoaer@gmail.com>
2023-05-21 20:39:30 +0800

    ci: 构建程序、下载和打包表情包合并为一个脚本

--- exec.sh
+++ exec.sh
@@ -6,3 +6,22 @@ fi
 
 # download emojis
 ./bilibili-stickers
+
+# make package dir
+if [ ! -d "./package" ]; then
+    mkdir ./package
+fi
+
+# copy executable file
+cp ./bilibili-stickers ./package/
+
+stickers=$(ls ./stickers)
+for sticker in $stickers; do
+    if [ -d ./stickers/$sticker ]; then
+        echo $sticker
+        if [ ! -f "$sticker.zip" ]; then
+            zip -r $sticker.zip ./stickers/$sticker
+        fi
+        mv $sticker.zip ./package/
+    fi
+done
