abab458b399659f4ae6d91f3633c3a8365b147ac
amtoaer <amtoaer@gmail.com>
2023-05-21 22:35:57 +0800

    chore: 似乎单表情包打包会由于文件数量过多出现 500 错误，合并为单个文件上传

--- exec.sh
+++ exec.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ ! -d "./bilibili-stickers" ]; then
+if [ ! -d "./sticker-downloader" ]; then
     # compile
-    go build -o bilibili-stickers -ldflags "-s -w" ./
+    go build -o sticker-downloader -ldflags "-s -w" ./
 fi
 
 # download emojis
-./bilibili-stickers
+./sticker-downloader
 
 # make package dir
 if [ ! -d "./package" ]; then
@@ -13,15 +13,8 @@ if [ ! -d "./package" ]; then
 fi
 
 # copy executable file
-cp ./bilibili-stickers ./package/
+mv ./sticker-downloader ./package/
 
-stickers=$(ls ./stickers)
-for sticker in $stickers; do
-    if [ -d ./stickers/$sticker ]; then
-        echo $sticker
-        if [ ! -f "$sticker.zip" ]; then
-            zip -r $sticker.zip ./stickers/$sticker
-        fi
-        mv $sticker.zip ./package/
-    fi
-done
+# zip stickers file
+zip -r ./package/stickers.zip ./stickers/
+
